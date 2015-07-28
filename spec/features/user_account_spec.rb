a559c3d28e7f984e53c4def843b86d20ccae6797
Michael Reeves <mreeves68@yahoo.com>
2015-07-28 17:03:23 -0600

    Implements DatabaseCleaner gem

--- spec/features/user_account_spec.rb
+++ spec/features/user_account_spec.rb
@@ -2,19 +2,28 @@ require 'rails_helper'
 
 feature "a user", type: :feature do
 
+  let(:user) do
+    user = User.new( first_name: 'Richard', last_name: 'Foo',
+                     username: 'rfoo', password: 'aaaaa' )
+  end
+
   it "can create an account" do
     visit new_user_path
 
     expect(current_path).to eq new_user_path
     expect(page).to have_content 'Create a New Account'
 
-    page.fill_in 'First name', with: 'Richard'
-    page.fill_in 'Last name',  with: 'Foo'
-    page.fill_in 'Username',   with: 'rfoo'
-    page.fill_in 'Password',   with: 'aaaaa'
+    page.fill_in 'First name', with: user.first_name
+    page.fill_in 'Last name',  with: user.last_name
+    page.fill_in 'Username',   with: user.username
+    page.fill_in 'Password',   with: user.password
     page.click_button 'Create Account'
 
     expect(current_path).to eq user_path(1)
+  end
+
+  it "can login" do
+    user.save
   end
 
 end
